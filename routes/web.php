0e8987848a6e996db6f8054abcf7349972338787
marco-gallegos <ma_galeza@hotmail.com>
2020-10-10 17:39:02 -0500

    [add] laravel 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,28 +13,6 @@
 |
 */
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/admin', function(){
-    return view('admin.home');
-});
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Route::group(['middleware' => ['auth']], function () {
-    
-    Route::resource('user', 'UserController');
-    Route::get('user/role/{id}', 'UserController@role');
-    Route::post('user/linkrole/{id}', "UserController@linkrole");
-    Route::get('user/permission/{id}', 'UserController@permission');
-    Route::post('user/linkpermission/{id}', "UserController@linkpermission");
-    
-    Route::resource('permission', 'PermissionController');
-    
-    Route::resource('role', 'RoleController');
-    //vincular permisos a los roles
-    Route::get('role/permissions/{id}', 'RoleController@permissions');
-    Route::post('role/link/{id}', "RoleController@link");
-
+Route::get('/', function () {
+    return view('welcome');
 });
